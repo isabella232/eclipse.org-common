a2113ef678e62a638bc02bc183c29c185bd7ccb7
Christopher Guindon <chris.guindon@eclipse.org>
2013-07-29 11:32:41 -0400

    update google search to use https

--- themes/Nova/menu.php
+++ themes/Nova/menu.php
@@ -1,4 +1,4 @@
-<? 
+<?
 /*******************************************************************************
  * Copyright (c) 2008-2012 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
@@ -12,19 +12,12 @@
 	global $App;
 	$www_prefix = "";
 	$pageRSS = "";
-	
-	$image_protocol = "http";
-	
-	if(isset($_SERVER['HTTPS'])) {
-		if($_SERVER['HTTPS']) {
-			$image_protocol = "https";
-		}
-	}
-	
-	
+
+	$http_protocol = $App->getHTTPPrefix();
+
 	if(isset($App)) {
 		$www_prefix = $App->getWWWPrefix();
-		
+
 		if(isset($App->PageRSS)) {
 			if($App->PageRSS != "") {
 				$pageRSS = $App->PageRSS;
@@ -35,26 +28,26 @@
 	ob_start();
 	for($i = 0; $i < $Menu->getMenuItemCount(); $i++) {
 		$MenuItem = $Menu->getMenuItemAt($i);
-		
+
 		?>
-		<li><a href="<?= $MenuItem->getURL(); ?>" target="<?= $MenuItem->getTarget(); ?>"><?= $MenuItem->getText(); ?></a></li> 
-	<? } 
+		<li><a href="<?= $MenuItem->getURL(); ?>" target="<?= $MenuItem->getTarget(); ?>"><?= $MenuItem->getText(); ?></a></li>
+	<? }
 	$menuHTML = ob_get_clean();
 	?>
-<div id="header">			
+<div id="header">
 	<div id="menu"><ul><?=$menuHTML;?></ul></div>
 	<div id="search">
-		<form action="http://www.google.com/cse" id="searchbox_017941334893793413703:sqfrdtd112s">
+		<form action="<?php print $http_protocol;?>://www.google.com/cse" id="searchbox_017941334893793413703:sqfrdtd112s">
 		<fieldset><input type="hidden" name="cx" value="017941334893793413703:sqfrdtd112s" />
 		<input id="searchBox" type="text" name="q" size="25" />
 		<input id="searchButton" type="submit" name="sa" value="Search" />
 		</fieldset></form>
-		<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=searchbox_017941334893793413703%3Asqfrdtd112s&amp;lang=en"></script>			
+		<script type="text/javascript" src="<?php print $http_protocol;?>h://www.google.com/coop/cse/brand?form=searchbox_017941334893793413703%3Asqfrdtd112s&amp;lang=en"></script>
 	</div>
 </div>
 	<? if ($Nav == NULL) { ?>
 	<div id="novaContent">
-	<? } 
+	<? }
 	else { ?>
 	<div id="novaContent" class="faux"><br id="faux-br" style="clear:both;height:1em;"/>
 	<? } ?>
