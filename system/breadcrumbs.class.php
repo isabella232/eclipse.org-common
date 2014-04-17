77c700366bbe4f4695d7129c9bbb83e1c3e7a5f6
Christopher Guindon <chris.guindon@eclipse.org>
2014-04-16 20:44:20 -0400

    add removeCrumb()
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- system/breadcrumbs.class.php
+++ system/breadcrumbs.class.php
@@ -12,11 +12,11 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menuitem.class.php");
 
 class Breadcrumb extends Menu {
-	
+
 	private $CrumbList = array();
-	
+
 	# static list of first-level URIs with corresponding display-friendly names
-	# everything outside of this is considered to be in project space 
+	# everything outside of this is considered to be in project space
 	private $FirstLevel = array(
 			"committers"	=> "Committers",
 			"downloads"		=> "Downloads",
@@ -30,16 +30,16 @@ class Breadcrumb extends Menu {
 	function getCrumbList() {
 		return $this->CrumbList;
 	}
-	
+
 	function setCrumbList($_List) {
 		$this->CrumbList = $_List;
 	}
-	
+
 	# Main constructor
 	function Breadcrumb() {
 
 		$www_prefix = "";
-		
+
 		global $App;
 
 		if(!isset($App)) {
@@ -50,13 +50,13 @@ class Breadcrumb extends Menu {
 
 		# Default: Home
 		$this->addCrumb("Home", $www_prefix . "/", "_self");
-		
+
 		if(isset($_SERVER['REQUEST_URI'])) {
 			http://www.eclipse.org/newsgroups/test.php
 			# Array ( [0] => [1] => newsgroups [2] => test.php )
 			$items = explode("/", $_SERVER['REQUEST_URI']);
-			
-			
+
+
 			# Examine Item 1 (first level URL)
 			if(isset($this->FirstLevel[$items[1]])) {
 				$this->addCrumb($this->FirstLevel[$items[1]], $www_prefix . "/" . $items[1], "_self");
@@ -67,18 +67,18 @@ class Breadcrumb extends Menu {
 				$this->addCrumb("Project", $www_prefix . "/projects/", "_self");
 				$this->addCrumb($items[1], $www_prefix . "/" . $items[1], "_self");
 			}
-			
+
 			# Add current page
 			# AT this point, $pageTitle should be set as we are running in header()
 			global $pageTitle;
 			if(isset($pageTitle)) {
 				$title = $pageTitle;
-				
+
 				# consider truncating $pageTitle if it's too long
 				if(strlen($title) > 35) {
 					$title = substr($title, 0, 35) . "...";
-				} 
-				
+				}
+
 				$this->addCrumb($pageTitle, NULL, NULL);
 			}
 			else {
@@ -87,11 +87,11 @@ class Breadcrumb extends Menu {
 			}
 		}
 	}
-	
+
 	function addCrumb($_Text, $_URL, $_Target) {
 		# Menu Items must be added at position 1
 		$Crumb = new Link($_Text, $_URL, $_Target, 0);
-			
+
 		# Add incoming menuitem
 		$this->CrumbList[count($this->CrumbList)] = $Crumb;
 	}
@@ -99,20 +99,20 @@ class Breadcrumb extends Menu {
 	function getCrumbCount() {
 		return count($this->CrumbList);
 	}
-	
+
 	function getCrumbAt($_Pos) {
 		if($_Pos < $this->getCrumbCount()) {
 			return $this->CrumbList[$_Pos];
 		}
 	}
-	
+
 	/**
 	 * Insert breadcrumb at a specific position
 	 * @param unknown $_Pos Position to insert at
 	 * @param unknown $_Text Link text
 	 * @param unknown $_URL Link URL
 	 * @param unknown $_Target Link target
-	 * 
+	 *
 	 */
 	function insertCrumbAt($_Pos, $_Text, $_URL, $_Target) {
 		if($_Pos < $this->getCrumbCount() && $_Pos > 0) {  # Don't allow inserting before Home
@@ -126,13 +126,49 @@ class Breadcrumb extends Menu {
 		}
 	}
 
+	/**
+   * Unset a link from CrumbList
+   *
+   * @param int|array $_Key
+   */
+  private function _removeCrumb($_Key){
+  	if (isset($this->CrumbList[$_Key])) {
+  		unset($this->CrumbList[$_Key]);
+  	}
+  }
+
+  /**
+   * Remove links from CrumbList
+   *
+   * @param int|array $_Key
+   */
+	function removeCrumb($_Key = NULL) {
+
+		if ($_Key == NULL) {
+      return;
+		}
+
+		if (is_array($_Key)) {
+      foreach ($_Key as $k) {
+      	$this->_removeCrumb($k);
+      }
+		}
+		else {
+			$this->_removeCrumb($_Key);
+		}
+
+		// 'reindex' the CrumbList array.
+		$this->CrumbList = array_values($this->CrumbList);
+
+	}
+
 	function showBreadcrumbs() {
 		# for debugging purposes only
 		echo "Breadcrumbs: ";
 		foreach ($this->CrumbList as $Crumb) {
 			# $Crumb is a Link object
 			echo "<a href='" . $Crumb->getURL() . "'>" . $Crumb->getText() . "</a>";
-			echo " | " ; 
+			echo " | " ;
 		}
 	}
 }
