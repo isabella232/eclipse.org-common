c38dc340cf0a5df3b58bb099b5775f9724efc069
Christopher Guindon <chris.guindon@eclipse.org>
2014-07-23 18:03:18 -0400

    fix whitespaces with starterkit
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- themes/solstice/docs/starterkit/_projectCommon.php
+++ themes/solstice/docs/starterkit/_projectCommon.php
@@ -10,14 +10,14 @@
  *    Christopher Guindon (Eclipse Foundation) - Initial implementation
  *******************************************************************************/
 
-	# Set the theme for your project's web pages.
-	# See the Committer Tools "Phoenix" secion in the How Do I? for list of themes
-	# https://dev.eclipse.org/committers/
-	$theme = "solstice";
+  # Set the theme for your project's web pages.
+  # See the Committer Tools "Phoenix" secion in the How Do I? for list of themes
+  # https://dev.eclipse.org/committers/
+  $theme = "solstice";
 
-	# Define your project-wide Nav bars here.
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank).
-	$Nav->addNavSeparator("Solstice", 	"/eclipse.org-common/themes/solstice/docs/");
-	$Nav->addCustomNav("Documentation", "/eclipse.org-common/themes/solstice/docs/", "_self", NULL);
-	$Nav->addCustomNav("Source code", "http://git.eclipse.org/c/www.eclipse.org/eclipse.org-common.git/tree/themes/solstice/", "_self", NULL);
-	$Nav->addCustomNav("Using Phoenix", "http://wiki.eclipse.org/Using_Phoenix", "_self", NULL);
+  # Define your project-wide Nav bars here.
+  # Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank).
+  $Nav->addNavSeparator("Solstice",   "/eclipse.org-common/themes/solstice/docs/");
+  $Nav->addCustomNav("Documentation", "/eclipse.org-common/themes/solstice/docs/", "_self", NULL);
+  $Nav->addCustomNav("Source code", "http://git.eclipse.org/c/www.eclipse.org/eclipse.org-common.git/tree/themes/solstice/", "_self", NULL);
+  $Nav->addCustomNav("Using Phoenix", "http://wiki.eclipse.org/Using_Phoenix", "_self", NULL);
