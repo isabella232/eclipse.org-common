9246e3af549ca4603d14509b0f71fa67e4e6ad08
Christopher Guindon <chris.guindon@eclipse.org>
2014-10-14 11:41:12 -0400

    Bug 446893 - Solstice's javascripts kill project-plan.php URL with anchor in Firefox
    Adding window.location.hash = href;
    
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- themes/solstice/src/javascript/main.js
+++ themes/solstice/src/javascript/main.js
@@ -1,14 +1,19 @@
-
+// main.js
 (function($, document) {
-	// main.js
-
+	var href_hash = window.location.hash;
 	// Add a class if right column is non-existant.
 	if ($("#rightcolumn").length == 0) {
 		$("#midcolumn").attr("class", "no-right-sidebar");
+		if (href_hash) {
+			href_hash = href_hash;
+		}
 	}
 	// add a class if left column is non-existant.
-	if($("#leftcol").length == 0) {
+	if ($("#leftcol").length == 0) {
 		$("#midcolumn").attr("class", "no-left-nav");
+		if (href_hash) {
+			href_hash = href_hash;
+		}
 	}
 	
 	// This code will prevent unexpected menu close when
@@ -16,7 +21,7 @@
 	$(document).on("click", ".yamm .dropdown-menu", function(e) {
 	  e.stopPropagation()
 	});
-	
+
 	// scroll button.
 	$(window).on("load resize scroll", function(){
 		if ($(window).width() < 1270){
