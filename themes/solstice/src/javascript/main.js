6dea2023fe024de2a60804e43fa4e01175f3667d
Christopher Guindon <chris.guindon@eclipse.org>
2014-10-14 11:01:50 -0400

    Bug 446893 - Solstice's javascripts kill project-plan.php URL with anchor in Firefox
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- themes/solstice/src/javascript/main.js
+++ themes/solstice/src/javascript/main.js
@@ -1,20 +1,20 @@
-// main.js
+
 (function($, document) {
+	// main.js
+
+	// Add a class if right column is non-existant.
+	if ($("#rightcolumn").length == 0) {
+		$("#midcolumn").attr("class", "no-right-sidebar");
+	}
+	// add a class if left column is non-existant.
+	if($("#leftcol").length == 0) {
+		$("#midcolumn").attr("class", "no-left-nav");
+	}
+	
 	// This code will prevent unexpected menu close when
 	// using some components (like accordion, forms, etc).
 	$(document).on("click", ".yamm .dropdown-menu", function(e) {
 	  e.stopPropagation()
-	});
-
-	$(document).ready(function() {
-		// Add a class if right column is non-existant.
-		if($("#rightcolumn").length == 0) {
-			$("#midcolumn").attr("class", "no-right-sidebar");
-		}
-		// add a class if left column is non-existant.
-		if($("#leftcol").length == 0) {
-			$("#midcolumn").attr("class", "no-left-nav");
-		}
 	});
 	
 	// scroll button.
