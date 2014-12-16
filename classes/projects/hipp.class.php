2a737745d6ad1aaf30a98c89aaf803a3b5eea786
Christopher Guindon <chris.guindon@eclipse.org>
2014-12-16 10:59:32 -0500

    Create HTML for hipp control panel link
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- classes/projects/hipp.class.php
+++ classes/projects/hipp.class.php
@@ -50,6 +50,38 @@ class Hipp {
 
   var $State = "";
 
+  /**
+   * Create HTML for hipp control panel link
+   *
+   * @param string $id
+   *   Project id (technology.cbi)
+   *
+   * @param string $shortname
+   *   Project shortname (cbi)
+   *
+   * @return string
+   */
+  public function getControlLink($id, $shortname){
+    $state = $this->getState();
+    $str = "<span id='" . $shortname . "_state' class='" . $state . "'>" . $state . "</span> &#160; <span id='" . $shortname . "_instance'>";
+
+    # Examine Service status to determine control knobs to place
+    if ($state == "running") {
+      # Add STOP button
+      $str .= '<a href="#ct" data-action="stop" data-projectid="' . $id . '" data-shortname="' . $shortname . '" class="hipp-control-action-link" ><i class="fa fa-stop"></i></a> ';
+
+      # Add RESTART button
+      $str .= '<a href="#ct" data-action="restart" data-projectid="' . $id . '" data-shortname="' . $shortname . '" class="hipp-control-action-link" ><i class="fa fa-refresh"></i></a>';
+    }
+
+    if ($state == "stopped") {
+      # Add START button
+      $str .= '<a href="#ct" data-action="start" data-projectid="' . $id . '" data-shortname="' . $shortname . '" class="hipp-control-action-link" ><i class="fa fa-play"></i></a>';
+    }
+
+    return $str . "</span>";
+  }
+
   function getID() {
     return $this->ID;
   }
