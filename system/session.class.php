6c0fc2755c1a93e8099e855c49076068bccace92
Christopher Guindon <chris.guindon@eclipse.org>
2015-01-21 13:34:35 -0500

    verify if the .htaccess file is writable before fopen
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- system/session.class.php
+++ system/session.class.php
@@ -231,7 +231,7 @@ class Session {
 
         # add session to the .htaccess file
         # TODO: implement a smart locking
-        if ($Friend->getIsBenefit()) {
+        if ($Friend->getIsBenefit() && is_writable($this->htaccess)) {
           $fh = fopen($this->htaccess, 'a') or die("can't open file");
           $new_line = "SetEnvIf Cookie \"" . $this->getGID() . "\" eclipsefriend=1\n";
           fwrite($fh, $new_line);
