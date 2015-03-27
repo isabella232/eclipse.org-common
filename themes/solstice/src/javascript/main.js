5521a74cb838b9981905277de7a5a58142f4ce3a
Christopher Guindon <chris.guindon@eclipse.org>
2015-03-27 11:12:32 -0400

    add hash when we click on a tab
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- themes/solstice/src/javascript/main.js
+++ themes/solstice/src/javascript/main.js
@@ -55,7 +55,7 @@
 	
 	var hash = window.location.hash;
 	hash && $('ul.nav a[href="' + hash + '"]').tab('show') && $('html,body').scrollTop(0);
-	$('.nav-tabs a').click(function (e) {
+	$('.nav.tabs a').click(function (e) {
 		$(this).tab('show');
 		window.location.hash = this.hash;
 		$('.alert').remove();
