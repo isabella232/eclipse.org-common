e2c06b90a0de7a51e832d21760f49d447677d5e3
Christopher Guindon <chris.guindon@eclipse.org>
2015-06-23 12:16:57 -0400

    Remove previous ads + add gradle and java9
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- classes/ads/promotedPlugin.class.php
+++ classes/ads/promotedPlugin.class.php
@@ -28,55 +28,21 @@ class PromotedPlugin extends EclipseAds {
    */
   public function PromotedPlugin($source = "") {
     parent::__construct($source);
-/*
+
     $Ad = new Ad();
-    $Ad->setBody('Your Eclipse is slow. Optimizer for Eclipse speeds up your IDE by
-      finding and fixing common configuration issues in your Eclipse installation.');
-    $Ad->setUrl('//marketplace.eclipse.org/marketplace-client-intro?mpc_install=2231050');
-    $Ad->setTitle('Optimizer for Eclipse');
-    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/OforE_marketplace-1.png');
-    $Ad->setCampaign('PP_OPTIMIZER');
+    $Ad->setTitle('Java 9 Support (Beta)');
+    $Ad->setUrl('http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=2393593');
+    $Ad->setBody('Early access to Java 9 support for Mars.');
+    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/default_images/default_2.png?itok=hA89-j9Y');
+    $Ad->setCampaign('PP_JAVA9');
     $this->newAd($Ad);
 
     $Ad = new Ad();
-    $Ad->setBody('Reactive Blocks lets you combine Java programming and visual
-      development to build robust and concurrent IoT gateway applications.');
-    $Ad->setUrl('http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=1776078');
-    $Ad->setTitle('Reactive Blocks');
-    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/reactive-blocks-marketplace_0.png');
-    $Ad->setCampaign('PP_REACTIVE');
-    $this->newAd($Ad);
-*/
-    $Ad = new Ad();
-    $Ad->setTitle('Gradle Integration');
-    $Ad->setUrl('http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=1799756');
-    $Ad->setBody('The Eclipse-Integration-Gradle project brings you developer tooling for Gradle into Eclipse.');
-    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/default_images/default_2.png');
+    $Ad->setTitle('Buildship Gradle Integration');
+    $Ad->setUrl('http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=2306961');
+    $Ad->setBody('Eclipse plug-ins that provide support for building software using Gradle.');
+    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/Gradle-Logo.png?itok=I76toUo1');
     $Ad->setCampaign('PP_GRADLE');
-    $this->newAd($Ad);
-
-    $Ad = new Ad();
-    $Ad->setTitle('MyEclipse Enterprise Workbench');
-    $Ad->setUrl('http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=69');
-    $Ad->setBody('Where developers unite to craft masterful code');
-    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/lowres_myeclipse_256x256.png');
-    $Ad->setCampaign('PP_MYECLIPSE');
-    $this->newAd($Ad);
-
-    $Ad = new Ad();
-    $Ad->setTitle('GlassFish Tools');
-    $Ad->setUrl('http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=2318313');
-    $Ad->setBody('Tools for developing applications for GlassFish.');
-    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/GlassFishToolsMarketplaceLogo_3.png');
-    $Ad->setCampaign('PP_GLASSFISH');
-    $this->newAd($Ad);
-
-    $Ad = new Ad();
-    $Ad->setTitle('JBoss Developer Studio');
-    $Ad->setUrl('http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=1616973');
-    $Ad->setBody('Includes support for JBoss and related technology; there are support for Hibernate, JBoss AS, CDI, Drools, jBPM, JSF, (X)HTML, Seam, Maven, JBoss Portal and more.');
-    $Ad->setImage('//marketplace.eclipse.org/sites/default/files/styles/ds_medium/public/devstudio_logo_132x95_0.png');
-    $Ad->setCampaign('PP_JBOSS');
     $this->newAd($Ad);
   }
 
