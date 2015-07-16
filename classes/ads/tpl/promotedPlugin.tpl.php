829871759e74506ce1aa6dcec28c2f032f8900de
Christopher Guindon <chris.guindon@eclipse.org>
2015-07-16 11:16:19 -0400

    Bug 1657 - Promoted plugin is broken on chrome
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- classes/ads/tpl/promotedPlugin.tpl.php
+++ classes/ads/tpl/promotedPlugin.tpl.php
@@ -12,32 +12,23 @@
 if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])){exit();}
 ?>
 <div class="promoted-plugin">
-  <div class="row">
-    <div class="col-md-24">
-      <div class="header">
-        <div class="row">
-          <div class="col-xs-6"><i class="fa fa-star">&nbsp;</i></div>
-          <div class="col-xs-18">
-            <h3>Promoted Plugin</h3>
-          </div>
-        </div>
-      </div>
-      <div class="content">
-        <div class="row">
-          <div class="col-xs-8"><img src="<?php print $this->ad->getImage();?>"></div>
-          <div class="col-xs-16">
-            <p><strong><?php print $this->ad->getTitle();?></strong></p>
-            <p><?php print $this->ad->getBody();?></p>
-          </div>
-        </div>
-      </div>
-      <div class="download clearfix drag_installbutton">
-        <a href="<?php print $this->ad->getUrl();?>" class="drag">
-          <i class="fa fa-download pull-left"></i>
-           <span class="padding-top-10" style="display:block">INSTALL NOW</span>
-          <span class="tooltip show-right"><span class="h3">Drag to Install!</span><br/>Drag to your running Eclipse workspace.</span>
-        </a>
-      </div>
-    </div>
-  </div>
+  <a href="<?php print $this->ad->getUrl();?>" class="drag">
+    <span class="header clearfix">
+      <h3><i class="fa fa-star">&nbsp;</i> Promoted Plugin</h3>
+    </span>
+    <span class="content clearfix">
+      <span class="col-xs-8 pp-image">
+        <img src="<?php print $this->ad->getImage();?>" class="img-responsive"/>
+      </span>
+      <span class="col-xs-16 pp-content">
+        <span class="pp-title"><?php print $this->ad->getTitle();?></span>
+        <span class="pp-text"><?php print $this->ad->getBody();?></span>
+      </span>
+    </span>
+    <span class="download clearfix drag_installbutton">
+      <i class="fa fa-download pull-left"></i>
+      <span class="padding-top-10" style="display:block">INSTALL NOW</span>
+      <span class="tooltip show-right"><span class="h3">Drag to Install!</span><br/>Drag to your running Eclipse workspace.</span>
+    </span>
+  </a>
 </div>
