f7721223cb49e7831aa0d6c7d2a2a6253314c431
Christopher Guindon <chris.guindon@eclipse.org>
2016-06-21 15:57:41 -0400

    Bug 496509 - Remove old database files from eclipse.org-common
    Change-Id: Ie2d4f2d180f3760f506f6e62ebc135e515d44fe8
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- system/smartconnection.class.php
+++ system/smartconnection.class.php
@@ -1,7 +1,6 @@
 <?php
- 
 /*******************************************************************************
- * Copyright (c) 2006 Eclipse Foundation and others.
+ * Copyright (c) 2006, 2016 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,16 +8,19 @@
  *
  * Contributors:
  *    Wayne Beaton + Nathan Gervais (Eclipse Foundation)- initial API and implementation
+ *    Christopher Guindon (Eclipse Foundation) - Bug 496509 - Remove old database files from eclipse.org-common
  *******************************************************************************/
 
-	if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php")) {
-		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php";
-		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_epic_ro.class.php";	
-		require_once "/home/data/httpd/eclipse-php-classes/system/dbconnection_live_rw.class.php";	
-	} else {
-		require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection.class.php");
-		//require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection_epic_ro.class.php");
-		//require_once ($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/dbconnection_live_rw.class.php");		
-	}
+  if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php")) {
+    require_once("/home/data/httpd/eclipse-php-classes/system/dbconnection.class.php");
+  }
 
-?>
+  // Possibly deprecated: Epic does not exist, will this connect to marketplace?
+  if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection_epic_ro.class.php")) {
+    require_once("/home/data/httpd/eclipse-php-classes/system/dbconnection_epic_ro.class.php");
+  }
+
+  // Possibly deprecated: live.eclipse.org does not exist, is this still needed?
+  if (file_exists("/home/data/httpd/eclipse-php-classes/system/dbconnection_live_rw.class.php")) {
+    require_once ("/home/data/httpd/eclipse-php-classes/system/dbconnection_live_rw.class.php");
+  }
