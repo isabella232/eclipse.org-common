9a99baad6affee20f0a78aeb79b4bd9fa3b83070
Christopher Guindon <chris.guindon@eclipse.org>
2016-06-22 14:14:02 -0400

    Verify if EvtLog class exist before defining it
    Change-Id: Ie8bbb77a0c39f7f0f97d8a43a3bbd64bea4efeaa
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- system/evt_log.class.php
+++ system/evt_log.class.php
@@ -27,130 +27,132 @@ else {
   define('LOG_TO_DB', FALSE);
 }
 
-class EvtLog {
+if (!class_exists ("EvtLog")) {
+  class EvtLog {
 
-  var $LogID = 0;
-  var $LogTable = "";
-  var $PK1 = "";
-  var $PK2 = "";
-  var $LogAction = "";
-  var $uid = "";
-  var $EvtDateTime = "";
+    var $LogID = 0;
+    var $LogTable = "";
+    var $PK1 = "";
+    var $PK2 = "";
+    var $LogAction = "";
+    var $uid = "";
+    var $EvtDateTime = "";
 
-  function getLogID() {
-    return $this->LogID;
-  }
+    function getLogID() {
+      return $this->LogID;
+    }
 
-  function getLogTable() {
-    return $this->LogTable;
-  }
+    function getLogTable() {
+      return $this->LogTable;
+    }
 
-  function getPK1() {
-    return $this->PK1;
-  }
+    function getPK1() {
+      return $this->PK1;
+    }
 
-  function getPK2() {
-    return $this->PK2;
-  }
+    function getPK2() {
+      return $this->PK2;
+    }
 
-  function getLogAction() {
-    return $this->LogAction;
-  }
+    function getLogAction() {
+      return $this->LogAction;
+    }
 
-  function getuid() {
-    return $this->PersonID;
-  }
+    function getuid() {
+      return $this->PersonID;
+    }
 
-  function getEvtDateTime() {
-    return $this->EvtDateTime;
-  }
+    function getEvtDateTime() {
+      return $this->EvtDateTime;
+    }
 
-  function setLogID($_LogID) {
-    $this->LogID = $_LogID;
-  }
+    function setLogID($_LogID) {
+      $this->LogID = $_LogID;
+    }
 
-  function setLogTable($_LogTable) {
-    $this->LogTable = $_LogTable;
-  }
+    function setLogTable($_LogTable) {
+      $this->LogTable = $_LogTable;
+    }
 
-  function setPK1($_PK1) {
-    $this->PK1 = $_PK1;
-  }
+    function setPK1($_PK1) {
+      $this->PK1 = $_PK1;
+    }
 
-  function setPK2($_PK2) {
-    $this->PK2 = $_PK2;
-  }
+    function setPK2($_PK2) {
+      $this->PK2 = $_PK2;
+    }
 
-  function setLogAction($_LogAction) {
-    $this->LogAction = $_LogAction;
-  }
+    function setLogAction($_LogAction) {
+      $this->LogAction = $_LogAction;
+    }
 
-  function setuid($_uid) {
-    $this->uid = $_uid;
-  }
+    function setuid($_uid) {
+      $this->uid = $_uid;
+    }
 
-  function setEvtDateTime($_EvtDateTime) {
-    $this->EvtDateTime = $_EvtDateTime;
-  }
+    function setEvtDateTime($_EvtDateTime) {
+      $this->EvtDateTime = $_EvtDateTime;
+    }
 
-  function insertModLog($_uid) {
-    $uid = $_uid;
-    if (LOG_TO_DB) {
-      if ($this->getLogTable() != "" && $this->getPK1() != "" && $this->getLogAction() != "" && $uid != "") {
+    function insertModLog($_uid) {
+      $uid = $_uid;
+      if (LOG_TO_DB) {
+        if ($this->getLogTable() != "" && $this->getPK1() != "" && $this->getLogAction() != "" && $uid != "") {
 
-        $App = new App();
-        $dbc = new DBConnectionRW();
-        $dbh = $dbc->connect();
+          $App = new App();
+          $dbc = new DBConnectionRW();
+          $dbh = $dbc->connect();
 
-        $sql = "INSERT INTO SYS_EvtLog (
-            LogID,
-            LogTable,
-            PK1,
-            PK2,
-            LogAction,
-            uid,
-            EvtDateTime)
-          VALUES (
-            NULL,
-            " . $App->returnQuotedString($App->sqlSanitize($this->getLogTable(), $dbh)) . ",
-            " . $App->returnQuotedString($App->sqlSanitize($this->getPK1(), $dbh)) . ",
-            " . $App->returnQuotedString($App->sqlSanitize($this->getPK2(), $dbh)) . ",
-            " . $App->returnQuotedString($App->sqlSanitize($this->getLogAction(), $dbh)) . ",
-            " . $App->returnQuotedString($App->sqlSanitize($uid), $dbh) . ",
-            NOW()
-          )";
+          $sql = "INSERT INTO SYS_EvtLog (
+              LogID,
+              LogTable,
+              PK1,
+              PK2,
+              LogAction,
+              uid,
+              EvtDateTime)
+            VALUES (
+              NULL,
+              " . $App->returnQuotedString($App->sqlSanitize($this->getLogTable(), $dbh)) . ",
+              " . $App->returnQuotedString($App->sqlSanitize($this->getPK1(), $dbh)) . ",
+              " . $App->returnQuotedString($App->sqlSanitize($this->getPK2(), $dbh)) . ",
+              " . $App->returnQuotedString($App->sqlSanitize($this->getLogAction(), $dbh)) . ",
+              " . $App->returnQuotedString($App->sqlSanitize($uid), $dbh) . ",
+              NOW()
+            )";
 
-        mysql_query($sql, $dbh);
-        if (mysql_error() != "") {
-          echo "An unknown database error has occurred while logging information.  Please contact the System Administrator.";
-          echo mysql_error();
-          exit();
+          mysql_query($sql, $dbh);
+          if (mysql_error() != "") {
+            echo "An unknown database error has occurred while logging information.  Please contact the System Administrator.";
+            echo mysql_error();
+            exit();
+          }
+
+          $dbc->disconnect();
+
+          // 1% of each hits will perform clean up
+          if (rand(0, 100) < 1) {
+            $this->cleanup();
+          }
         }
-
-        $dbc->disconnect();
-
-        // 1% of each hits will perform clean up
-        if (rand(0, 100) < 1) {
-          $this->cleanup();
+        else {
+          echo "An unknown system error has occurred while logging information.  Please contact the System Administrator.";
+          exit();
         }
       }
       else {
-        echo "An unknown system error has occurred while logging information.  Please contact the System Administrator.";
-        exit();
+        // TODO: local logging
       }
     }
-    else {
-      // TODO: local logging
+
+    function cleanup() {
+      $sql = "DELETE FROM SYS_EvtLog WHERE EvtDateTime < " . MAX_LOG_DAYS;
+
+      $dbc = new DBConnectionRW();
+      $dbh = $dbc->connect();
+      mysql_query($sql, $dbh);
+      $dbc->disconnect();
     }
+
   }
-
-  function cleanup() {
-    $sql = "DELETE FROM SYS_EvtLog WHERE EvtDateTime < " . MAX_LOG_DAYS;
-
-    $dbc = new DBConnectionRW();
-    $dbh = $dbc->connect();
-    mysql_query($sql, $dbh);
-    $dbc->disconnect();
-  }
-
 }
