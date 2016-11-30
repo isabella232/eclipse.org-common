a9d5935a2fe34d65f18c2dc1b549a193217bfdd2
Eric Poirier <eric@eclipse.org>
2016-11-30 14:35:03 -0500

    Infra 2090 - Fix for takemeback on login menu item
    Change-Id: I607ed10fa743653fefbbb8d8a09f30156ba4c549
    Signed-off-by: Eric Poirier <eric@eclipse.org>

--- classes/themes/baseTheme.class.php
+++ classes/themes/baseTheme.class.php
@@ -1731,6 +1731,25 @@ EOHTML;
   }
 
   /**
+   * Returns a Take me back
+   *
+   * @return string
+   */
+  private function _getTakeMeBack() {
+    // Return an empty string if we're on the dev.eclipse.org website
+    if (strpos($_SERVER['SERVER_NAME'], "dev.eclipse") !== FALSE) {
+      return "";
+    }
+
+    $path = parse_url($this->App->getCurrentURL(), PHP_URL_PATH);
+    if (substr($path, 0, 1) == "/") {
+      $path = substr($path, 1);
+    }
+
+    return "?takemeback=" . $this->getBaseUrl() . $path;
+  }
+
+  /**
    * Get $ession_variables
    *
    * @param string $id
@@ -1747,7 +1766,7 @@ EOHTML;
       $Session = $this->_getSession();
       $Friend = $Session->getFriend();
       $this->session_variables['create_account_link'] = '<a href="' . $this->getBaseUrlLogin() . '/site_login/createaccount.php"><i class="fa fa-user fa-fw"></i> Create account</a>';
-      $this->session_variables['my_account_link'] = '<a href="' . $this->getBaseUrlLogin() . '/site_login/?takemeback=' . $this->getBaseUrl() . $_SERVER['REQUEST_URI'] . '"><i class="fa fa-sign-in fa-fw"></i> Log in</a>';
+      $this->session_variables['my_account_link'] = '<a href="' . $this->getBaseUrlLogin() . '/site_login/' . $this->_getTakeMeBack() . '"><i class="fa fa-sign-in fa-fw"></i> Log in</a>';
       $this->session_variables['logout'] = '';
 
       if ($Session->isLoggedIn()) {
