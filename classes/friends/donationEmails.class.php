0e578a965633ac53b820eac18717fa4608d1dedd
Eric Poirier <eric.poirier@eclipse-foundation.org>
2017-05-30 08:49:13 -0400

    Infra 2151 - Changes to the 35USD email text
    Change-Id: I8dadf3335c0e337905651a762166185f2196f760
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

--- classes/friends/donationEmails.class.php
+++ classes/friends/donationEmails.class.php
@@ -265,7 +265,7 @@ class DonationEmails {
     $query_string = http_build_query($query);
     $email = $this->_get_email_greeting_string();
     $email .= "Thank you for your donation. Your support is greatly appreciated and your donation will help make the Eclipse Platform even better for millions of developers around the world." . PHP_EOL . PHP_EOL;
-    $email .= "You’ve donated more than 35 USD, which means that you are now a Friend of Eclipse (FoE)!  As a Friend of Eclipse you will now have access to the FoE mirror site and a Friend badge for your Eclipse Account. Use this personalized link to login into your Eclipse Account to get access to the mirror site and the badge: https://" . $domain['domain'] . "/donate/link-account.php?" . $query_string . PHP_EOL . PHP_EOL;
+    $email .= "You’ve donated more than 35 USD, which means that you are now a Friend of Eclipse (FoE)! You will now be identified as a FoE with a Friend badge for your Eclipse Account and Bugzilla. Use this personalized link to login into your Eclipse Account to get access to the badge: https://" . $domain['domain'] . "/donate/link-account.php?" . $query_string . PHP_EOL . PHP_EOL;
     $email .= "Finally, as a Friend, you also get 40% off print & 50% off ebooks at oreilly.com using the discount code " . $this->_get_email_code('OREILLY') . PHP_EOL . PHP_EOL;
     $email .= $this->_get_email_footer_string();
     $this->email_content = $email;
