4647023a8081574b4ae4dc694ed0af2aadba1145
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2020-09-30 13:49:09 -0400

    [hot-fix] - fix js race condition
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- classes/themes/baseTheme.class.php
+++ classes/themes/baseTheme.class.php
@@ -334,7 +334,7 @@ class BaseTheme {
     $this->setAttributes('footer4', 'footer-other', 'id');
 
     // Default theme js file
-    $this->setAttributes('script-theme-main-js', $this->getThemeUrl('solstice') . 'public/javascript/main.min.js?var=0.0.119-2', 'src');
+    $this->setAttributes('script-theme-main-js', $this->getThemeUrl('solstice') . 'public/javascript/main.min.js?var=0.0.119-3', 'src');
   }
 
   /**
