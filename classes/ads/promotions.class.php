50eae5926ff5d739e85be4833293e204ae4268a0
Eric Poirier <eric.poirier@eclipse-foundation.org>
2020-12-18 08:37:56 -0500

    Add promotion test for JS plugin
    Change-Id: I5e8024631c41c41d7320615838d1f347bc04dca4
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

--- classes/ads/promotions.class.php
+++ classes/ads/promotions.class.php
@@ -158,13 +158,13 @@ class Promotions {
       'weight' => 4
     );
 
-    /*$promos[] = array(
+    $promos[] = array(
       'url' => 'PROMO_ENABLING_TRANSFORMATION_JS',
       'imageurl' => '/membership/promo/images/enabling_digital_transformation.png',
       'memberName' => 'EclipseFoundation',
       'type' => 'strat_ad',
-      'weight' => 2
-    );*/
+      'weight' => 0
+    );
 
     $promos[] = array(
       'url' => 'PROMO_JAKARTA_EE_DEV_SURVEY_2020',
